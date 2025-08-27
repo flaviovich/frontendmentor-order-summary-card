a7c91e830985560b8ac5140a9d16b5d274006f60
Flavio Rios <110625215+flaviovich@users.noreply.github.com>
2025-08-27 17:13:38 +0200

    refactor: update layout and styles for improved responsiveness and visual consistency

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,17 +2,6 @@
 module.exports = {
   theme: {
     extend: {
-      boxShadow: {
-        'primary': '0 10px 20px rgba(56, 41, 224, 0.15)',
-      },
-      colors: {
-        'blue-50': 'hsl(225, 100%, 98%)',
-        'blue-100': 'hsl(225, 100%, 94%)',
-        'blue-hover': 'hsl(245, 75%, 65%)',
-        'blue-700': 'hsl(245, 75%, 52%)',
-        'blue-950': 'hsl(223, 47%, 23%)',
-        'gray-50': 'hsl(224, 23%, 55%)',
-      },
     },
   },
 };
